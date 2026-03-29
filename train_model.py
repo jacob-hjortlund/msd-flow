68698729687b2ab19f860cf061a8f879c9371a49
jacob-hjortlund <jacob.hjortlund@gmail.com>
2026-03-29 15:28:56 +0200

    feat: wire ClearML task into train_model.py entry point

--- train_model.py
+++ train_model.py
@@ -8,6 +8,7 @@ import src.data as data
 from hydra.utils import call, instantiate
 from omegaconf import DictConfig, OmegaConf
 from src.utils import register_all_resolvers
+from src.tracking import setup_task
 
 register_all_resolvers()
 log = logging.getLogger(__name__)
@@ -16,7 +17,8 @@ log = logging.getLogger(__name__)
 @hydra.main(version_base=None, config_path="./configs", config_name="config")
 def main(cfg: DictConfig):
 
-    # 0. TODO: clearML setup
+    # 0. ClearML setup
+    task = setup_task(cfg.clearml)
 
     # 1. Variables and logging
     seed = cfg.seed
@@ -26,8 +28,7 @@ def main(cfg: DictConfig):
 
     # 2. Download data
     log.info("--- Step 2: Data Download ---")
-    download_cfg = cfg.data.download
-    call(download_cfg)
+    call(cfg.data.download)(clearml_task=task)
 
     # 3. Assign splits
     log.info("--- Step 3: Split Assignment ---")
@@ -52,7 +53,7 @@ def main(cfg: DictConfig):
     model_key, rng_key = jr.split(rng_key)
     model = instantiate(cfg.model)(key=model_key)
 
-    # Train model
+    # 6. Train model
     log.info("--- Step 6: Model Training ---")
     train_key, rng_key = jr.split(rng_key)
     trained_model = call(cfg.train)(
@@ -60,6 +61,11 @@ def main(cfg: DictConfig):
         model=model,
         dataloader=train_loader,
         val_dataloader=val_loader,
+        clearml_task=task,
+        sample_fn=instantiate(cfg.train.sample_fn) if cfg.train.sample_fn else None,
+        sample_every=cfg.train.sample_every,
+        num_samples=cfg.train.num_samples,
+        samples_dir=cfg.train.samples_dir,
     )
 
 
