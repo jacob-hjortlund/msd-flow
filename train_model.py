0a2c146f14fb15ddde2dc9a88d4a932bff2a9956
jacob-hjortlund <jacob.hjortlund@gmail.com>
2026-03-30 16:01:58 +0200

    minor changes to defaults, now downloading full dataset as default

--- train_model.py
+++ train_model.py
@@ -6,8 +6,8 @@ import jax.random as jr
 from hydra.utils import instantiate, call
 from omegaconf import DictConfig, OmegaConf, open_dict
 
-from msdflow.utils import register_all_resolvers
 from msdflow.tracking import setup_task
+from msdflow.utils import register_all_resolvers
 from msdflow.data.pipeline import resolve_dataset
 
 
